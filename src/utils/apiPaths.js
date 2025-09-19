cbe3df08a85d7a8d88ce913664d68d0d89140df7
njnbwrai <njngaming16@gmail.com>
2025-09-19 17:29:19 +0530

    Url Changed

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:8000";
-// export const BASE_URL = "https://the-bodoland-backend.onrender.com";
+// export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://the-bodoland-backend.onrender.com";
 
 export const API_PATHS = {
   AUTH: {
